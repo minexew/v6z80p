7e1616537542d223458aa250d16c5cb7d52d24a7
valencoder <valencoder@0f88cdff-d996-47aa-905b-e4b647ba1e89>
2010-07-13 16:20:48 +0000

    - updated set_stack.h

--- trunk/Contributions/Valen/c_programs/inc/set_stack.h
+++ trunk/Contributions/Valen/c_programs/inc/set_stack.h
@@ -1,2 +1,7 @@
+#ifndef SET_STACK_H
+#define SET_STACK_H
+
 #define SET_STACK()     const word own_sp = OWN_SP;     // OWN_SP is defined in project makefile
 SET_STACK()                                             // set stack address for this program (actual value is in project makefile)
+
+#endif /* SET_STACK_H */
