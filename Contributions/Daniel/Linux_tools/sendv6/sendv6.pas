ddd8f41c539990b1f1ef91cc73211354d80c5e82
insane-altair <insane-altair@0f88cdff-d996-47aa-905b-e4b647ba1e89>
2010-08-28 10:04:25 +0000

    Modified sendv6 to cope with sercopy v2.1

--- Contributions/Daniel/Linux_tools/sendv6/sendv6.pas
+++ Contributions/Daniel/Linux_tools/sendv6/sendv6.pas
@@ -4,14 +4,23 @@ uses
     
 const
   hdr:array[0..11] of char='Z80P.FHEADER';
+  ttydev:string='USB0';
 var
   ser: TBlockSerial;
   quit:boolean=false;
+  sendstat:boolean;
   buffer:array[0..257] of byte;
   fbuffer:array[0..16777215] of byte;
   fbufpos,
   flen:dword;
+  fname,
+  devstring:string;
   f:file;
+
+function isesc:boolean;
+begin
+  if keypressed then isesc:=readkey=#27 else isesc:=false;
+end;
 
 function calccrc:word;
 var
@@ -44,7 +53,7 @@ var s:array[0..15] of char;
 begin
   fillchar(buffer,258,0);
   fillchar(s,16,0);
-  s:=paramstr(1);
+  s:=fname;
   move(s,buffer[0],16);
   move(flen,buffer[16],4);
   move(hdr,buffer[$14],12);
@@ -55,45 +64,52 @@ var
   chk:word;
   chok:array[0..1] of char;
 begin
-  chk:=calccrc;
+  chk:=calccrc;chok:='??';
   move(chk,buffer[256],2);
   ser.sendbuffer(@buffer[0],258);
   ser.flush;
-  chok[0]:=char(ser.recvbyte(2000));
-  chok[1]:=char(ser.recvbyte(2000));
+  repeat
+    chok[0]:=chok[1];
+    chok[1]:=char(ser.recvbyte(2000));
+    if isesc then exit;
+  until ((chok='OK') or (chok='XX'));
   sendbuf:=chok='OK';
 end;
 
 begin
-  if paramcount<>1 then halt;
-  filemode:=0;
-  assign(f,paramstr(1));
-  reset(f,1);
-  flen:=filesize(f);
-  blockread(f,fbuffer,flen);
-  close(f);
-  fbufpos:=0;
+  if ((paramcount<1) or (paramcount>2))   then 
+  begin
+    writeln('Usage: SENDV6 <device> file');
+    writeln('use "SENDV6 file.asm" to send file.asm to /dev/ttyUSB0');
+    writeln('use "SENDV6 USB1 file.asm" to send file.asm to /dev/ttyUSB1');
+    halt;
+  end;
+  if paramcount=1 then fname:=paramstr(1) else begin fname:=paramstr(2); ttydev:=paramstr(1);end;
+  if not fileexists(fname) then begin writeln('?File not Found ERROR');halt; end;
+  filemode:=0; assign(f,fname); reset(f,1); flen:=filesize(f); blockread(f,fbuffer,flen); close(f); fbufpos:=0;
   ser:=TBlockSerial.Create;
+  devstring:='/dev/tty'+ttydev;
   try
-    ser.Connect('/dev/ttyUSB0');
+    ser.Connect(devstring);
     ser.config(115200, 8, 'N', SB1, False, False);
     makehdr;
     if sendbuf then
     begin
       while not quit do
       begin
+        clreol;write('sending to '+devstring+':',fbufpos,'/',flen);gotoxy(1,wherey);
         if fbufpos>flen then quit:=true else
         begin
           move(fbuffer[fbufpos],buffer[0],256);
           inc(fbufpos,256);
-          quit:=not sendbuf;
+          sendstat:=sendbuf;
+          quit:=not sendstat;
         end;
-        if keypressed then if readkey=#27 then quit:=true;
-        write('.');
+        if isesc then quit:=true;
       end;  
     end;
   finally
     ser.free;
   end;
-  writeln;
+  clreol;write('sending to '+devstring+':');if sendstat then writeln('done') else writeln('error');
 end.
