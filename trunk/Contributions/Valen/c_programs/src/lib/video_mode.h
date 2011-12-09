8e575b05dac7ec5b94ab09ca8cd13855c2890baf
valencoder <valencoder@0f88cdff-d996-47aa-905b-e4b647ba1e89>
2011-12-09 15:36:19 +0000

    deleted dir 'lib'

--- trunk/Contributions/Valen/c_programs/src/lib/video_mode.h
+++
@@ -1,5 +0,0 @@
-// Setup display window size
-void VideoMode_SetupDisplayWindowSize(byte window_x_start,  byte window_x_stop,
-			              byte window_y_start,  byte window_y_stop);
-void VideoMode_InitTilemapMode(byte additionalBits_vidctrl, byte additionalBits_ext_vidctrl);
-void TileMap_8x8_FillPlayfield(BYTE playfieldNumber, WORD offset, WORD fillSize, WORD tileNumber);
