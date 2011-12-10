7b09ffe42f61b71917e1da408b3bd47a4b6b368f
valencoder <valencoder@0f88cdff-d996-47aa-905b-e4b647ba1e89>
2011-12-10 12:27:32 +0000

    renamed   dir misc_lib to base_lib

--- trunk/Contributions/Valen/c_programs/c_support/misc_lib/file_operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_OPERATIONS_H
-#define FILE_OPERATIONS_H
-
-BOOL FileOp_LoadFileToBuffer(char *pFilename, dword file_offset, byte* buf, dword len, byte bank);
-BOOL FileOp_FLOS_FindFile(FLOS_FILE* const pFile, const char* pFilename);
-BOOL FileOp_FLOS_ForceLoad(const byte* address, const byte bank);
-
-
-#endif /* FILE_OPERATIONS_H */
