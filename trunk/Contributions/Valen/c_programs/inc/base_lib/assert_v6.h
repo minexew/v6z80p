96a171605a56ebe4bc9f02fd15fa01626445d986
valencoder <valencoder@0f88cdff-d996-47aa-905b-e4b647ba1e89>
2011-12-10 13:40:23 +0000

    base_lib refactoring

---
+++ trunk/Contributions/Valen/c_programs/inc/base_lib/assert_v6.h
@@ -0,0 +1,15 @@
+#ifndef ASSERT_V6_H
+#define ASSERT_V6_H
+
+// To enable assert functionality, define DEBUG in your program.
+
+#ifdef DEBUG
+#define ASSERT_V6(x) ((x) == 0 ? _assert_v6(#x, __FILE__, __LINE__):(void)0)
+void _assert_v6(char *expr, const char *filename, unsigned int linenumber);
+#else
+#define ASSERT_V6(x) ((void)0)
+#endif
+
+
+
+#endif /* ASSERT_V6_H */
